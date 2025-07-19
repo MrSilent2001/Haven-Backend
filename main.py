ea231c66156a87d1ef03231a5514090abd66b39c
Sachintha-Lakruwan <sachi.new2002@gmail.com>
2025-07-19 09:18:34 +0530

    add no copyright sounds into static folder and make ncsmusic route to retrieve available songs

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from util.db import client
 from routes.mood import router as mood_router
 from routes.user import router as user_router
 from routes.meditation import router as meditation_router
+from routes.ncsmusic import router as ncsmusic_router
 
 app = FastAPI()
 
@@ -13,8 +15,11 @@ DB_NAME = os.getenv("DB_NAME")
 
 db = client.get_database(DB_NAME)
 
+# Mount static files
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 # Pass db to routers if needed (or set up in each module)
 app.include_router(mood_router, prefix="/mood")
 app.include_router(user_router, prefix="/user")
 app.include_router(meditation_router, prefix="/meditation")
-
+app.include_router(ncsmusic_router, prefix="/ncsmusic")
